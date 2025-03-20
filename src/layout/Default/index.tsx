06dd6fc07611d86419a68deda6da4bd9f42e0565
ze-pena <jose.pena.dev@gmail.com>
2025-03-20 00:12:14 -0300

    refactor: clear default layout to work with header component

--- src/layout/Default/index.tsx
+++ src/layout/Default/index.tsx
@@ -1,24 +1,20 @@
 // Componentes
-import Header from '../../components/global/Header';
+import Header from '../../components/Elements/Header';
 
-// Header data
-import { menuData } from '../../data/menu';
+// Component data
+import { navigation } from '../../data/navigation';
 
 // Styling
 import './styles.sass';
 
 // Typing
-type Props = React.PropsWithChildren;
+// type Props = React.PropsWithChildren
 
 // Component
-function Default({ children }: Props) {
+function Default(/* { children }: Props */) {
   return (
     <div className="default">
-      <header className="default__header">
-        <Header menuList={menuData.menuList} />
-      </header>
-
-      <main className="default__main">{children}</main>
+      <Header data={navigation} />
     </div>
   );
 }
