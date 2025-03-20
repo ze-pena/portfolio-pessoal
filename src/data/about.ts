50a48f9e8af282b3423bbf5d46371e4f4e29395e
ze-pena <jose.pena.dev@gmail.com>
2025-03-19 23:54:15 -0300

    feat: create about data file

---
+++ src/data/about.ts
@@ -0,0 +1,33 @@
+// Common used types
+type Contact = {
+  label: string;
+  link: string;
+  type: 'mail' | 'phone' | 'linkedin' | 'github';
+};
+
+type Info = {
+  label: string;
+  type: 'local' | 'age';
+};
+
+// Main types
+type About = {
+  contactList: Array<Contact>;
+  infoList: Array<Info>;
+};
+
+// Main data
+const about: About = {
+  contactList: [
+    { label: 'Email', link: 'jose.pena.dev@gmail.com', type: 'mail' },
+    { label: 'Whatsapp', link: '+5511942008326', type: 'phone' },
+    { label: 'Linkedin', link: 'https://www.linkedin.com/in/ze-pena', type: 'linkedin' },
+    { label: 'Github', link: 'https://www.github.com/ze-pena', type: 'github' },
+  ],
+  infoList: [
+    { label: 'São Paulo', type: 'local' },
+    { label: '27 anos', type: 'age' },
+  ],
+};
+
+export { about };
