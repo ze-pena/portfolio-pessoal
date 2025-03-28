1c676537156a20aa1d549571fdab3cc3781125f5
ze-pena <jose.pena.dev@gmail.com>
2025-03-28 18:31:03 -0300

    feat: add "Content Academic" content component to the main app component

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@
 import Default from './layout/Default';
 import ContentAbout from './components/contents/ContentAbout';
 import ContentExperience from './components/contents/ContentExperience';
+import ContentAcademic from './components/contents/ContentAcademic';
 
 // Component data
 import { aboutData } from './data/About';
 import { experienceData } from './data/Experience';
+import { academicData } from './data/Academic';
 
 // Context
 import { useSettingsContext } from './context/Settings/context';
@@ -18,11 +20,13 @@ function App() {
   const settingsContext = useSettingsContext();
   const about = useTranslation(aboutData, settingsContext.context.language);
   const experience = useTranslation(experienceData, settingsContext.context.language);
+  const academic = useTranslation(academicData, settingsContext.context.language);
 
   return (
     <Default>
       <ContentAbout data={about} />
       <ContentExperience data={experience} />
+      <ContentAcademic data={academic} />
     </Default>
   );
 }
