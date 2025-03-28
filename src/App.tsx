26b827da1a4ce0a924466ffbcffdbeabfd23b6dc
ze-pena <jose.pena.dev@gmail.com>
2025-03-27 23:38:22 -0300

    feat: add "Content Experience" content to the main app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 // Components
 import Default from './layout/Default';
 import ContentAbout from './components/contents/ContentAbout';
+import ContentExperience from './components/contents/ContentExperience';
 
 // Component data
 import { aboutData } from './data/About';
+import { experienceData } from './data/Experience';
 
 // Context
 import { useSettingsContext } from './context/Settings/context';
@@ -15,10 +17,12 @@ import { useTranslation } from './hooks/useTranslation';
 function App() {
   const settingsContext = useSettingsContext();
   const about = useTranslation(aboutData, settingsContext.context.language);
+  const experience = useTranslation(experienceData, settingsContext.context.language);
 
   return (
     <Default>
       <ContentAbout data={about} />
+      <ContentExperience data={experience} />
     </Default>
   );
 }
