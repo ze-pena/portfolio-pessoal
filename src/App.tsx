cc21355d7b7f840724729ad8f41db22341863bae
ze-pena <jose.pena.dev@gmail.com>
2025-03-31 13:19:54 -0300

    feat: add new section content ""ContentSkills" on the main component app

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import Default from './layout/Default';
 import ContentAbout from './components/contents/ContentAbout';
 import ContentExperience from './components/contents/ContentExperience';
 import ContentAcademic from './components/contents/ContentAcademic';
+import ContentSkills from './components/contents/ContentSkills';
 
 // Component data
 import { aboutData } from './data/About';
 import { experienceData } from './data/Experience';
 import { academicData } from './data/Academic';
+import { skillData } from './data/Skills';
 
 // Context
 import { useSettingsContext } from './context/Settings/context';
@@ -21,12 +23,14 @@ function App() {
   const about = useTranslation(aboutData, settingsContext.context.language);
   const experience = useTranslation(experienceData, settingsContext.context.language);
   const academic = useTranslation(academicData, settingsContext.context.language);
+  const skill = useTranslation(skillData, settingsContext.context.language);
 
   return (
     <Default>
       <ContentAbout data={about} />
       <ContentExperience data={experience} />
       <ContentAcademic data={academic} />
+      <ContentSkills data={skill} />
     </Default>
   );
 }
