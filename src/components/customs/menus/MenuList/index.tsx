2c5c1cec4d8ac44b26815f4bb4f82122e811a0bb
ze-pena <jose.pena.dev@gmail.com>
2025-04-03 20:56:17 -0300

    feat: create a new component property to handle diferent styling

--- src/components/customs/menus/MenuList/index.tsx
+++ src/components/customs/menus/MenuList/index.tsx
@@ -9,12 +9,13 @@ type Option = {
 
 type Props = {
   menuList: Array<Option>;
+  orientation: 'horizontal' | 'vertical';
 };
 
 // Component
 function MenuList(props: Props) {
   return (
-    <menu className="menu-list">
+    <menu className={`menu-list --${props.orientation}`}>
       {props.menuList.map((menuItem, index) => (
         <li key={index} className="menu-list__item">
           <a href={menuItem.link}>{menuItem.label}</a>
