7a8ffb97d280429b8f4c3a0331d6dc31670e8b2b
ze-pena <jose.pena.dev@gmail.com>
2025-04-03 21:01:01 -0300

    feat: manage menu visibility on button click

--- src/components/global/Header/index.tsx
+++ src/components/global/Header/index.tsx
@@ -1,10 +1,16 @@
+// Dependencias
+import { useMemo } from 'react';
+
 // Components
 import MenuList from '@components/customs/menus/MenuList';
 import MenuSettings from '@components/customs/menus/MenuSettings';
 
+// Context
+import { useSettingsContext } from '@context/Settings/context';
+
 // Icon
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faClose } from '@fortawesome/free-solid-svg-icons';
 
 // Styling
 import './styles.sass';
@@ -14,22 +20,37 @@ import { HeaderData } from '@data/Header';
 
 type Props = {
   data: HeaderData;
+  position: 'fixed' | 'sticky';
 };
 
 // Component
 function Header(props: Props) {
+  const settingsContext = useSettingsContext();
+
+  const buttonState = useMemo(() => {
+    const {
+      context: { aside },
+    } = settingsContext;
+    return aside === 'menu' ? 'close' : 'open';
+  }, [settingsContext]);
+
+  const clickMenu = (): void => {
+    const { setContext } = settingsContext;
+    setContext(state => ({ ...state, aside: state.aside !== 'menu' ? 'menu' : 'none' }));
+  };
+
   return (
-    <header className="header">
+    <header className={`header --${props.position}`}>
       <div className="header__content">
         <div className="header__content__actions">
-          <button type="button" className="header__content__actions__button">
-            <FontAwesomeIcon icon={faBars} />
+          <button type="button" className={`header__content__actions__button --${buttonState}`} onClick={clickMenu}>
+            <FontAwesomeIcon icon={buttonState === 'open' ? faBars : faClose} />
           </button>
         </div>
 
         <div className="header__content__menu">
           <nav className="header__content__menu__list">
-            <MenuList menuList={props.data.menuList} />
+            <MenuList menuList={props.data.menuList} orientation="horizontal" />
           </nav>
 
           <div className="header__content__menu__settings">
